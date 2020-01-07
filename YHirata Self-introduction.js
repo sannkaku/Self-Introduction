1f68e0a60f776652cb8d70b69e85d9dc190f8e8d
Ypsirom <48702672+sannkaku@users.noreply.github.com>
2020-01-07 15:01:54 +0900

    more functional jQuery
    toggle colors,

--- YHirata Self-introduction.js
+++ YHirata Self-introduction.js
@@ -1,18 +1,32 @@
 $(function(){
-	//特性の長所と短所、プルダウンメニュー作成
+
+	//特性の長所と短所、プルダウンメニュー作成 My strong and weak points from my traits. Pull down menu.
 	$(".pulldown_menu").on("click", function() {
 		$(this).next().slideToggle();
 	});
-	
-	
-	//各都市の画像の紹介文機能作成
+
+	//プルダウンの色変え Color change when introduction subjects toggle
+	$(".pulldown_purple").click(function(){
+		$(this).toggleClass("pulldown_purple_click"); //(12.18 toggleするにはどうしよう？ => https://programmercollege.jp/column/9599/)
+	});
+
+	$(".pulldown_blue").click(function(){
+		$(this).toggleClass("pulldown_blue_click"); //(12.18 toggleするにはどうしよう？ => https://programmercollege.jp/column/9599/)
+	});
+
+	$(".pulldown_red").click(function(){
+		$(this).toggleClass("pulldown_red_click"); //(12.18 toggleするにはどうしよう？ => https://programmercollege.jp/column/9599/)
+	});
+
+
+	//各都市の画像の紹介文機能作成 / Introductions of each city
 	$("#munich").click(munich);
 	$("#stockholm").click(stockholm);
 	$("#london").click(london);
 	$("#newyork").click(newyork);
-	
-	
-	//(!WARNING!)クリックで変わる部分まではいいのだが、日本語だと文字化けが起きてしまう。
+
+
+	//(!WARNING!)クリックで変わる部分まではいいのだが、日本語だと文字化けが起きてしまうので、英語のみ対応。
 	function munich(){
 		$("#image_cities_click").text("Gute Reise nach --- Munich").css("color","#804000");
 	}
